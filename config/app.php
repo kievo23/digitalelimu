4e73a48b6face337d2ecfffb6396ffb4f769ab3d
kievo23 <kelvinchege@gmail.com>
2017-02-27 15:27:51 +0300

    add the app edit name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'MacMilan Publishers',
+    'name' => 'Digital Elimu',
 
     /*
     |--------------------------------------------------------------------------
