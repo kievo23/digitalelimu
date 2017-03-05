671ba7808b39f4ed36a06efbc43186232abb7390
kievo23 <kelvinchege@gmail.com>
2017-03-05 22:24:14 +0300

    uploading working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,9 +119,6 @@
     $(document).ready(function(){
         $('textarea').ckeditor({
             filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
-            filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}'
           });
 
         
