c266146825999c2a4fc19967989ccc39cc043f0e
kievo23 <kelvinchege@gmail.com>
2017-06-26 14:45:15 +0300

    set this to live

--- public/pesapal/Pesapal-iframe.php
+++ public/pesapal/Pesapal-iframe.php
@@ -61,7 +61,7 @@ if(isset($_POST['submit'])){
 </head>
 <body style="">
 	<div class="container">
-		<iframe src="<?php echo $iframe_src;?>" width="100%" height="700px"  scrolling="no" frameBorder="0">
+		<iframe src="<?php echo $iframe_src;?>" width="100%" height="100%"  scrolling="no" frameBorder="0">
 			<p>Browser unable to load iFrame</p>
 		</iframe>
 	</div>		
