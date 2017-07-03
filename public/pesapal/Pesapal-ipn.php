b6c652b3845b5b4ba126cce1a3c1fae2f27627c0
kievo23 <kelvinchege@gmail.com>
2017-07-03 23:28:51 +0300

    pesapal

--- public/pesapal/Pesapal-ipn.php
+++ public/pesapal/Pesapal-ipn.php
@@ -78,7 +78,7 @@ if($pesapalTrackingId!='')
           // set the PDO error mode to exception
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-          $stmt = $conn->prepare("SELECT id FROM clients where phone='".$client."'"); 
+          $stmt = $conn->prepare("SELECT id FROM clients where phone='0710345130'"); 
           echo $stmt;
           $stmt->execute();
           $client_id = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
