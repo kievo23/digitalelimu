98c7ce0106e3f20424b6eb8ee71305138e81bf30
kievo23 <kelvinchege@gmail.com>
2017-07-04 16:46:11 +0300

    pdf

--- resources/views/books/pdf.blade.php
+++ resources/views/books/pdf.blade.php
@@ -2,8 +2,9 @@
 @section('content')
 
 <?php
-print_r($book);
+print_r($book->pdf);
 ?>
+{{ url('pdf/'.$book->pdf) }}
 <iframe src="http://docs.google.com/gview?url=http://139.59.187.229/{{ url('pdf/'.$book->pdf) }}&embedded=true" style="width:100%; height:900px;" frameborder="0"></iframe>
 
 @endsection
