d3323ec560834aa23e5ee8b5611ff2bfc765b570
kievo23 <kelvinchege@gmail.com>
2017-07-04 16:50:37 +0300

    pdf

--- resources/views/books/pdf.blade.php
+++ resources/views/books/pdf.blade.php
@@ -5,6 +5,6 @@
 print_r($book->pdf);
 ?>
 {{ url('pdf/'.$book->pdf) }}
-<iframe src="http://docs.google.com/gview?url=http://139.59.187.229/{{ url('pdf/'.$book->pdf) }}&embedded=true" style="width:100%; height:900px;" frameborder="0"></iframe>
+<iframe src="https://docs.google.com/viewer?url=http://139.59.187.229/{{ url('pdf/'.$book->pdf) }}&embedded=true" style="width:100%; height:900px;" frameborder="0"></iframe>
 
 @endsection
