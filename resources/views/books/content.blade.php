2b91552fd4812e9bedbf72d7e235646520682836
kievo23 <kelvinchege@gmail.com>
2017-07-09 16:49:02 +0300

    submit changes

--- resources/views/books/content.blade.php
+++ resources/views/books/content.blade.php
@@ -3,10 +3,11 @@
 
 <!--
 	<input type="range" min="80" max="180" id="slider"  value="110" style="position:fixed;top:10px;"/>
-<div style="padding-top:30px">
+	-->
+<div style="padding-top:0px">
 	{!! $content !!}	
 </div>
--->
+
 
 @endsection
 @section('js')
