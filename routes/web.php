d3644ae3c8b3da5cc5785eb0333d1ce10d5cc314
kievo23 <kelvinchege@gmail.com>
2018-01-13 18:26:19 +0300

    safaricom api wallet

--- routes/web.php
+++ routes/web.php
@@ -131,7 +131,7 @@ Route::post('/api/readBook','ApiController@readBook');
 Route::post('/api/stkpush','ApiController@stkpush');
 Route::post('/api/stkresponse/{bookid}','ApiController@stkresponse');
 
-Route::post('/api/stkloadwalletpush/{phone}/{accesstoken}','ApiController@stkloadwalletpush');
+Route::get('/api/stkloadwalletpush/{phone}/{accesstoken}','ApiController@stkloadwalletpush');
 Route::post('/api/stkloadwalletresponse','ApiController@stkloadwalletresponse');
 
 //PesaPal, Unconventional
