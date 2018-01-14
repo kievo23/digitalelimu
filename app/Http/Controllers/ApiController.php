83596fe6a478c4f93598dfb681a4184c29280dd0
kievo23 <kelvinchege@gmail.com>
2018-01-14 19:08:43 +0300

    stk push

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -493,7 +493,7 @@ class ApiController extends Controller
         $phone = $callbackData->Body->stkCallback->CallbackMetadata->Item[4]->Value;
 
         $payments = new Payments();
-        $payments->transcode = date("Y-m-d H:i:s")."@".$bookid;
+        $payments->transcode = date("Y-m-d H:i:s")."@";
         $payments->category = "weewr";
         $payments->providerRefId = $callbackData->Body->stkCallback->CallbackMetadata->Item[1]->Value;
         $payments->source = "Safaricom";
