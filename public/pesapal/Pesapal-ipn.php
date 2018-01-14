12d060606d1c4afc933e3f93fe36e1c3389126a5
kievo23 <kelvinchege@gmail.com>
2018-01-14 23:55:05 +0300

    stk push

--- public/pesapal/Pesapal-ipn.php
+++ public/pesapal/Pesapal-ipn.php
@@ -84,18 +84,14 @@ if($pesapalTrackingId!='')
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
           $stmt = $conn->prepare("SELECT id FROM clients where phone=".$client); 
-          print_r($stmt);
           $stmt->execute();
-          $client_id = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-          echo "Client Id<br>";
-          print_r($client_id);
+          $client = $stmt->fetch(PDO::FETCH_ASSOC); 
+          $client_id = $client['id'];
 
           $sql = "INSERT INTO subscriptions (client_id, book_id, amount, created_at, updated_at)
           VALUES ($client_id, $book, '$amount', '$today', '$today')";
           // use exec() because no results are returned
           $rst = $conn->exec($sql);
-          echo "Result<br>";
-          print_r($rst);
           echo "<h2>Successfully Subscribed to this book</h2>";
           }
       catch(PDOException $e)
