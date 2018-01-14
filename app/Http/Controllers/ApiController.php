d8faccd4c72caf225901add652a615cf78def047
kievo23 <kelvinchege@gmail.com>
2018-01-14 17:48:29 +0300

    stk push

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -507,8 +507,6 @@ class ApiController extends Controller
         $client = Clients::wherePhone("0".substr($phone,-9))->first();
 
         $sub = Wallet::whereClientId($client->id)->first();
-        print_r($sub);
-        exit();
         $sub->amount = $amount + $sub->amount;
         $sub->save();
         $callbackData=$mpesa->finishTransaction();
