3583c152b465f2bc54b2483a9f1eaa856f449156
kievo23 <kelvinchege@gmail.com>
2018-01-26 18:22:58 +0300

    get books with wallet balance

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-    	'api/payments','api/readBook','api/registerUser','api/stkpush','api/stkresponse/*','api/stkloadwalletresponse','api/walletstkpush','api/stkresponseclass/*','api/stkpushclass'
+    	'api/payments','api/readBook','api/registerUser','api/stkpush','api/stkresponse/*','api/stkloadwalletresponse','api/walletstkpush','api/stkresponseclass/*','api/stkpushclass','api/stkloadwalletpushclass'
         //
     ];
 }
