22fc9e6807d20fcc84ab6b5604c6f6a5356f979a
kievo23 <kelvinchege@gmail.com>
2018-02-03 21:18:15 +0300

    digital elimu pesapal

--- public/pesapal/Pesapal-iframeclass.php
+++ public/pesapal/Pesapal-iframeclass.php
@@ -5,7 +5,7 @@ include_once('OAuth.php');
 session_start();
 $classid = $_GET['class_id'];
 $client = $_GET['client'];
-print_r($_GET);
+//print_r($_GET);
 
 if(isset($_POST['submit'])){
 	//pesapal params
