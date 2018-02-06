1aa992ee723e445f9a24a68ea964d62d952ed560
kievo23 <kelvinchege@gmail.com>
2018-02-06 16:23:10 +0300

    digital elimu

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -91,7 +91,7 @@ class BookController extends Controller
         }
         
         if($request->file('bookpdf')){
-            $fileName = str_slug($request->file('bookpdf')->getClientOriginalName().rand(111,999), ".");
+            $fileName = str_slug($request->file('bookpdf')->getClientOriginalName().rand(11,99), ".");
             $upload = $request->file('bookpdf')->move('pdf/', $fileName);
             $topic->bookpdf = strtolower($fileName);
         }
@@ -99,7 +99,7 @@ class BookController extends Controller
             $pdf = "";
             $files = $request->file('pdf');
             foreach ($files as $file) {
-                $fileName = str_slug(rand(11111,99999).$file->getClientOriginalName().rand(111,999), ".");
+                $fileName = str_slug($file->getClientOriginalName().rand(11,99), ".");
                 $upload = $file->move('pdf/', $fileName);
                 $pdf .= $fileName.",";                
             }
@@ -189,7 +189,7 @@ class BookController extends Controller
         }
         $files = $request->file('pdf');
         if($request->file('bookpdf')){
-            $fileName = str_slug(rand(11111,99999).$request->file('bookpdf')->getClientOriginalName(), ".");
+            $fileName = str_slug($request->file('bookpdf')->getClientOriginalName().rand(11,99), ".");
             $upload = $request->file('bookpdf')->move('pdf/', $fileName);
             $topic->bookpdf = strtolower($fileName);
         }
